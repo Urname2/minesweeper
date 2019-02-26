5b291d416273c18e38f15cdea45753786ac1c6f1
Jørgen Brække <jorgenhb@live.com>
2019-02-26 16:18:47 +0100

    Add files via upload
    init

---
+++ mine.test.js
@@ -0,0 +1,23 @@
+function mapMaker(input) {
+
+    var map = [];
+
+    input.forEach((row) => {
+        for (let index = 0; index < row.length; index++) {
+            const cell = row[index];
+            if (cell == '.') {
+                map.push('0');
+            } else {
+                map.push('1');
+            }
+        }
+    })
+
+    return map;
+}
+
+test('mapMaker creates one row, without bombs', () => {
+    var input = ['.'];
+    const fact = ['0'];
+    expect(mapMaker(input)).toEqual(fact);
+});
