422dacffc7239e75662b6902949f1161ca4bf858
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2019-02-26 16:36:50 +0100

    Some more code

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 2) installer node (https://nodejs.org/en/)
 
 3) npm install
+
+4) npm run watch
+
+5) change the line `test.skip('mapMaker shows bombs'`, change `test.skip` to `test`
+
+6) watch the test fail
+
+7) make it pass
--- mine.test.js
+++ mine.test.js
@@ -1,23 +1,29 @@
 function mapMaker(input) {
 
-    var map = [];
+  var map = [];
 
-    input.forEach((row) => {
-        for (let index = 0; index < row.length; index++) {
-            const cell = row[index];
-            if (cell == '.') {
-                map.push('0');
-            } else {
-                map.push('1');
-            }
-        }
-    })
+  input.forEach((row) => {
+    for (let index = 0; index < row.length; index++) {
+      const cell = row[index];
+      if (cell == '.') {
+        map.push('0');
+      } else {
+        map.push('1');
+      }
+    }
+  })
 
-    return map;
+  return map;
 }
 
 test('mapMaker creates one row, without bombs', () => {
-    var input = ['.'];
-    const fact = ['0'];
-    expect(mapMaker(input)).toEqual(fact);
+  var input = ['.'];
+  const fact = ['0'];
+  expect(mapMaker(input)).toEqual(fact);
+});
+
+test.skip('mapMaker shows bombs', () => {
+  var input = ['*'];
+  const fact = ['*'];
+  expect(mapMaker(input)).toEqual(fact);
 });
